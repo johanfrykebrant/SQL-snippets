8185dc0a6f3a15a7e1103c900aad8357a9647958
Johan Frykebrant <85884666+johanfrykebrant@users.noreply.github.com>
2022-09-30 09:59:46 +0200

    Create InvestigateSsisPackages.sql

---
+++ InvestigateSsisPackages.sql
@@ -0,0 +1,13 @@
+SELECT
+	b.package_name,
+	b.executable_name,
+	a.execution_path,
+	a.execution_duration/60000.00 AS DurationInMinutes,
+	CONVERT(VARCHAR(20), CONVERT(DATETIME2, a.start_time), 120) AS StartTime,
+	CONVERT(VARCHAR(20), CONVERT(DATETIME2, a.end_time), 120) AS EndTime
+FROM [SSISDB].[catalog].[executable_statistics] a
+	LEFT JOIN internal.executables b ON a.executable_id = b.executable_id
+WHERE CONVERT(DATETIME2, start_time) >= 'YYYY-HH-MM hh:mm'
+	AND b.package_name = <some_package>
+	AND b.executable_name like '%<some_executable>%'
+ORDER BY StartTime DESC,EndTime ASC
