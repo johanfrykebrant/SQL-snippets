896536524626e7f5b13cf8f526215578c011e237
Johan Frykebrant <85884666+johanfrykebrant@users.noreply.github.com>
2022-11-18 09:19:49 +0100

    Update InvestigateSsisPackages.sql

--- InvestigateSsisPackages.sql
+++ InvestigateSsisPackages.sql
@@ -7,7 +7,7 @@ SELECT
 	CONVERT(VARCHAR(20), CONVERT(DATETIME2, a.end_time), 120) AS EndTime
 FROM [SSISDB].[catalog].[executable_statistics] a
 	LEFT JOIN internal.executables b ON a.executable_id = b.executable_id
-WHERE CONVERT(DATETIME2, start_time) >= 'YYYY-HH-MM hh:mm'
+WHERE CONVERT(DATETIME2, StartTime) >= 'YYYY-HH-MM hh:mm'
 	AND b.package_name = <some_package>
 	AND b.executable_name like '%<some_executable>%'
 ORDER BY StartTime DESC,EndTime ASC
