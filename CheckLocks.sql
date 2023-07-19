57ee249dd1ec3de035ebf2cd161f3996ae47d129
Johan Frykebrant <85884666+johanfrykebrant@users.noreply.github.com>
2023-07-19 13:11:43 +0200

    Update CheckLocks.sql

--- CheckLocks.sql
+++ CheckLocks.sql
@@ -1,10 +1,24 @@
-SELECT resource_type, resource_associated_entity_id,
-     request_status, request_mode,request_session_id,
-     resource_description, o.object_id, o.name, o.type_desc
-FROM sys.dm_tran_locks l, sys.objects o
-WHERE l.resource_associated_entity_id = o.object_id
-    and resource_database_id = DB_ID()
-
-
-SELECT *
-FROM sys.dm_exec_sessions
+SELECT l.resource_type
+    , l.resource_associated_entity_id
+    , l.request_status
+    , l.request_mode
+    , request_session_id
+	, ex.session_id
+    , l.resource_description
+    , o.object_id
+    , o.name
+    , o.type_desc
+    , ex.client_interface_name
+    , ex.login_name
+    , ex.STATUS
+    , ex.cpu_time
+    , ex.memory_usage
+    , ex.total_scheduled_time
+    , ex.total_elapsed_time
+    , ex.last_request_start_time
+    , ex.last_request_end_time
+FROM sys.dm_tran_locks l
+LEFT JOIN sys.dm_exec_sessions ex
+    ON l.request_session_id = ex.session_id
+LEFT JOIN sys.objects o
+    ON l.resource_associated_entity_id = o.object_id
